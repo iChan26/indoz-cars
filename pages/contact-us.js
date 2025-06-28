502a0126512c3db01572c0bb88b3fe890c487df1
ardit <penaleschristian45@gmail.com>
2025-06-28 23:40:24 +0800

    updates

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -910,13 +910,18 @@ const isActive = router.pathname === '/contact-us';
         </select>
       </div>
 
-      {/* Message */}
-      <div className="col-span-1 md:col-span-2">
-        <label className="block text-sm font-medium text-gray-700 mb-1">
-          Message <span className="text-red-500">*</span>
-        </label>
-        <textarea rows="5" className="w-full border border-gray-300 rounded px-3 py-2 resize-none"></textarea>
-      </div>
+  {/* Message */}
+<div className="col-span-1 md:col-span-2">
+  <label className="block text-sm font-medium text-gray-700 mb-1">
+    Message <span className="text-red-500">*</span>
+  </label>
+  <textarea
+    rows="5"
+    className="w-full border border-gray-300 rounded px-3 py-2 resize-y focus:outline-none focus:ring-2 focus:ring-yellow-600"
+    placeholder=""
+  ></textarea>
+</div>
+
 
       {/* Consent */}
       <div className="col-span-1 md:col-span-2">
